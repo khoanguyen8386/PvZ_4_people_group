210897bb1c9a6602d369f0397a33b24e510e68ac
Nguyen <ititiu24039@student.hcmiu.edu.vn>
2025-06-08 20:23:52 +0700

    Update BasicZombie.java

--- BasicZombie.java
+++ BasicZombie.java
@@ -26,7 +26,7 @@ public class BasicZombie extends Zombie
         armlesseat = importSprites("armlesszombieeating", 7);
         armless = importSprites("armlesszombie", 7);
         walkSpeed = Random.Double(11, 14);
-        maxHp = 200;
+        maxHp = 150;
         hp = maxHp;
     }
 
