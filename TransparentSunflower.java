f8da54455f52bb6c162e3c10ae68989e5c76596e
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:17:09 +0700

    Update TransparentSunflower.java

--- TransparentSunflower.java
+++ TransparentSunflower.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class TransparentSunflower here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class TransparentSunflower extends TransparentObject
 {
-    /**
-     * Act - do whatever the TransparentSunflower wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+   
     public TransparentSunflower(boolean bool) {
         if (bool) {
             getImage().setTransparency(125);
