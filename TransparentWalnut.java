fe503e9e5dc29c8e0ec894debd61b3b313c2ee28
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:16:58 +0700

    Update TransparentWalnut.java

--- TransparentWalnut.java
+++ TransparentWalnut.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class TransparentWalnut here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*; 
 public class TransparentWalnut extends TransparentObject
 {
-    /**
-     * Act - do whatever the TransparentWalnut wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public TransparentWalnut(boolean bool) {
         if (bool) {
             getImage().setTransparency(125);
