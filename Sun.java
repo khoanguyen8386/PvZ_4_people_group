26913128f0c2f0f6478ebad8fd60dd2f76c99701
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:21:00 +0700

    Update Sun.java

--- Sun.java
+++ Sun.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Sun here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class Sun extends FallingObject
 {
-    /**
-     * Act - do whatever the Sun wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public MyWorld MyWorld;
     public GreenfootImage[] sun;
     public boolean clicked = false;
