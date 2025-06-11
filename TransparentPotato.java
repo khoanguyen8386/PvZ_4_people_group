bab4d03011f6dc07bdf88219854747a860f5cdf0
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:18:02 +0700

    Update TransparentPotato.java

--- TransparentPotato.java
+++ TransparentPotato.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class TransparentPotato here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class TransparentPotato extends TransparentObject
 {
-    /**
-     * Act - do whatever the TransparentPotato wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+   
     public TransparentPotato(boolean bool) {
         if (bool) {
             getImage().setTransparency(125);
@@ -21,6 +11,6 @@ public class TransparentPotato extends TransparentObject
     }
     public void act()
     {
-        // Add your action code here.
+       
     }
 }
