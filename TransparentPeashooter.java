7adab803161db2c687d7d218cd01e7c0653dddb0
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:18:13 +0700

    Update TransparentPeashooter.java

--- TransparentPeashooter.java
+++ TransparentPeashooter.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class TransparentPeashooter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class TransparentPeashooter extends TransparentObject
 {
-    /**
-     * Act - do whatever the TransparentPeashooter wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+   
     public TransparentPeashooter(boolean bool) {
         if (bool) {
             getImage().setTransparency(125);
