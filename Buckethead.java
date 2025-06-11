51283e6f91af7ada5c78c915c7539befbc1123a6
VLXDpog <Daungocduc2006@gmail.com>
2025-06-11 10:05:26 +0700

    Adding ZombieBucket

--- Buckethead.java
+++ Buckethead.java
@@ -1,17 +1,6 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class BasicZombie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class Buckethead extends Zombie
 {
-    /**
-     * Act - do whatever the BasicZombie wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public boolean bucket = true;
     public GreenfootImage[] idle;
     public GreenfootImage[] walk;
