35b10e4dd5967bab238ed1c8d81d893b95fece0e
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:22:26 +0700

    Update SeedPacket.java

--- SeedPacket.java
+++ SeedPacket.java
@@ -1,18 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class SeedPacket here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class SeedPacket extends Actor
 {
-    /**
-     * Act - do whatever the SeedPacket wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
+   
     public long deltaTime;
     public long deltaTime2;
     public long lastFrame = System.nanoTime();
