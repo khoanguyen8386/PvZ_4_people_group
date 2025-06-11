61edd46f9af506f6116119570fa9a6216c698c7c
VLXDpog <Daungocduc2006@gmail.com>
2025-06-11 10:01:25 +0700

    Changing status basic zombie

--- BasicZombie.java
+++ BasicZombie.java
@@ -1,17 +1,6 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class BasicZombie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class BasicZombie extends Zombie
 {
-    /**
-     * Act - do whatever the BasicZombie wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     
     public GreenfootImage[] idle;
     public GreenfootImage[] walk;
