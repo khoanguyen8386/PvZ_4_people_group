0d62796cbb1753e0fc092ec89d1df3560f3acbf6
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:18:42 +0700

    Update TransparentObject.java

--- TransparentObject.java
+++ TransparentObject.java
@@ -1,23 +1,13 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class TransparentObject here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class TransparentObject extends Actor
 {
-    /**
-     * Act - do whatever the TransparentObject wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public TransparentObject() {
         
     }
     public void act()
     {
-        // Add your action code here.
+       
     }
     public void setTransparent(boolean bool) {
         if (bool) {
