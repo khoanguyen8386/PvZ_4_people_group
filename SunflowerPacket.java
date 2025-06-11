912655548e8c3deb79b5ec2488fc9ae0a6071768
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:19:26 +0700

    Update SunflowerPacket.java

--- SunflowerPacket.java
+++ SunflowerPacket.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class SunflowerPacket here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class SunflowerPacket extends SeedPacket
 {
-    /**
-     * Act - do whatever the SunflowerPacket wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+  
     public SunflowerPacket() {
         super(8000L, true,50, "sunflowerpacket");
     }
