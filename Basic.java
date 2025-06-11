b7fe205f46539254c7c2372f216227776c86f5d2
VLXDpog <Daungocduc2006@gmail.com>
2025-06-11 09:59:26 +0700

    BasicZombie status update

--- Basic.java
+++ Basic.java
@@ -1,17 +1,6 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Basic here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class Basic extends IdleZombie
 {
-    /**
-     * Act - do whatever the Basic wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     GreenfootImage[] idle;
     public Basic() {
         idle = importSprites("zombieidle", 4);
