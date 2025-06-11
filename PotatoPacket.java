8208a99f336da8c833d6614a2fc76c6a5d1c328c
VLXDpog <Daungocduc2006@gmail.com>
2025-06-11 10:20:56 +0700

    PotatoSeed display

--- PotatoPacket.java
+++ PotatoPacket.java
@@ -1,17 +1,6 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class PotatoPacket here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*; 
 public class PotatoPacket extends SeedPacket
 {
-    /**
-     * Act - do whatever the PotatoPacket wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public PotatoPacket() {
         super(40000L, false, 25, "potatopacket");
     }
