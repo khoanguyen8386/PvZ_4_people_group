7ed2220b26d2d0fbf8093d3a3c8859acbc6fcb13
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:23:14 +0700

    Update Retry.java

--- Retry.java
+++ Retry.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Retry here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class Retry extends Button
 {
-    /**
-     * Act - do whatever the Retry wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public World restart;
     public Retry(World restart) {
         super("retry1.png", "retry2.png");
