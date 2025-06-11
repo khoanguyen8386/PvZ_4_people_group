b0eb087cef2ad0d959aed715f24014099e123c22
vannhan262 <vannhan26022006@gmail.com>
2025-06-11 10:23:38 +0700

    Update Repeater.java

--- Repeater.java
+++ Repeater.java
@@ -1,16 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Repeater here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class Repeater extends Plant
 {
-    /**
-     * Act - do whatever the Repeater wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.  */
+    
     private GreenfootImage[] idle;
     private GreenfootImage[] shoot;
     private boolean shootOnce = false;
