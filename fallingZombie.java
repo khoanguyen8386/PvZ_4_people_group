0b91fe0b5c3c41d005f8e08d7556b69c55c3c362
Nguyen <ititiu24039@student.hcmiu.edu.vn>
2025-06-11 09:57:23 +0700

    Update fallingZombie.java

--- fallingZombie.java
+++ fallingZombie.java
@@ -1,17 +1,9 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; 
 
-/**
- * Write a description of class fallingZombie here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class fallingZombie extends animatedObject
 {
-    /**
-     * Act - do whatever the fallingZombie wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+   
     public GreenfootImage[] fall;
     
     
